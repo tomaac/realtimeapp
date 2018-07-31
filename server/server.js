796313229583aa3c6fa6c9ab1140d72d8925f226
matiss <001@syndicate.lv>
2018-07-31 10:00:23 +0300

    starting app

---
+++ server/server.js
@@ -0,0 +1,32 @@
+
+  const express = require('express');
+  const path = require('path');
+  const publicPath = path.join(__dirname, '..', '/public');
+  const port = process.env.PORT || 3000;
+
+
+  var app = express();
+  app.use(express.static(publicPath));
+  app.listen(port, function(){
+     console.log('server is up on :'+port);
+  });
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // end
